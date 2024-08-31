be7b06c7d7fb4fc2bef77517685d2af2d238e55e
magnaituvshin <maaagaaab1999@gmail.com>
2024-09-01 00:39:05 +1000

    first try

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': 'ff5d7c7e18msh786dea9999c2618p1b08a2jsn38d7d455ea67',
+  'x-rapidapi-host': 'bing-web-search1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://bing-web-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
